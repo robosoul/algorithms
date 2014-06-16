4edda40261d09cf5ef3c016e33401b5008e14ed7
robosoul <obradovic.luka.83@gmail.com>
2014-06-16 13:12:53 +0200

    union-find lectures examples

---
+++ src/main/java/org/hoshi/uf/UF.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright Vast 2014. All Rights Reserved.
+ *
+ * http://www.vast.com
+ */
+package org.hoshi.uf;
+
+/**
+ * @author Luka Obradovic (luka@vast.com)
+ */
+public interface UF {
+}
---
+++ src/test/java/org/hoshi/uf/QuickFindUFTest.java
@@ -0,0 +1,34 @@
+package org.hoshi.uf;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class QuickFindUFTest {
+
+    @BeforeMethod
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void testUnion() throws Exception {
+
+    }
+
+    @Test
+    public void testConnected() throws Exception {
+
+    }
+
+    @Test
+    public void testFind() throws Exception {
+
+    }
+
+    @Test
+    public void testCount() throws Exception {
+
+    }
+}
